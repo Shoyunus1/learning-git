c213f958d3cdfdc2586bdf7ae59fc5f6e76c094a
Shoyunus1 <46818682+Shoyunus1@users.noreply.github.com>
2022-02-10 17:34:13 +0500

    resolved conflict

--- utils.js
+++ utils.js
@@ -1,2 +1,17 @@
-// ToDo: Implements utils
-//
+if (true) {
+    var varVariable = "thid is true ";
+
+}
+
+var varVariable = "this is false";
+
+console.log(varVariable);
+
+if (true) {
+    let letVariable = "true";
+    let letVariable = "false";
+    console.log(letVariable);
+}
+
+let letVariable = "false";
+console.log(letVariable);
